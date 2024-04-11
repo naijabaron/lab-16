2fb2acc233095ea2b362ab5e3eacc0677fe79ba8
samuelokon <166614105+naijabaron@users.noreply.github.com>
2024-04-11 20:38:10 +0300

    solved 1st task

---
+++ lab16_solutions.c
@@ -0,0 +1,29 @@
+//
+// Created by HP on 4/11/2024.
+//
+
+#include "lab16_solutions.h"
+#include "libs/data_structures/matrix/matrix.h"
+#include <minmax.h>
+#include "libs/util/util.h"
+#include <math.h>
+
+void swapMaxElementRowAndMinElementRow(matrix matrix) {
+    int min = INT_MAX, minRow = -1, max = 0, maxRow = -1;
+
+    for (int i = 0; i < matrix.rows; ++i) {
+        for (int j = 0; j < matrix.columns; ++j) {
+            int value = matrix.cells[i][j];
+
+            if (value < min) {
+                min = value;
+                minRow = i;
+            } else if (value > max) {
+                max = value;
+                maxRow = i;
+            }
+        }
+    }
+
+    swapRows(matrix, minRow, maxRow);
+}
---
+++ lab16_solutions.h
@@ -0,0 +1,10 @@
+//
+// Created by HP on 4/11/2024.
+//
+
+#ifndef LAB_16_LAB16_SOLUTIONS_H
+#define LAB_16_LAB16_SOLUTIONS_H
+
+void swapMaxElementRowAndMinElementRow(matrix matrix);
+
+#endif //LAB_16_LAB16_SOLUTIONS_H
